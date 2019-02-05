fb0c058cf2d24a5fbcac86dbfe81643834cb416c
Omar Barbosa <omar.barbosa89@outlook.com>
2019-02-04 20:28:48 -0500

    Add link to post list in blog page

--- resources/views/templates/item.blade.php
+++ resources/views/templates/item.blade.php
@@ -1,11 +1,11 @@
 <div class="row blog-list align-items-center mb-2">
     <div class="col-lg-2 col-xl-2 d-none d-lg-block d-xl-block">
-        <a href="#"><img class="img-fluid mb-5" src="{{ url($post->featured_image) }}" alt="{{ $post->excerpt }}"></a>
+        <a href="{{ route('posts.article', ['slug' => $post->slug]) }}"><img class="img-fluid mb-5" src="{{ url($post->featured_image) }}" alt="{{ $post->excerpt }}"></a>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 align-items-center">
-        <a href="#"><h1 class="text-uppercase blog-list-title">{{ $post->title }}</h1></a>
-            <p class="text-muted"><i class="fas fa-calendar"></i> {{ $latest->created_at->toDateString() }} </p>
-        <a href="#">
+        <a href="{{ route('posts.article', ['slug' => $post->slug]) }}"><h1 class="text-uppercase blog-list-title">{{ $post->title }}</h1></a>
+            <p class="text-muted"><i class="fas fa-calendar"></i> {{ $post->created_at->toDateString() }} </p>
+        <a href="{{ route('posts.article', ['slug' => $post->slug]) }}">
             <h2 class="font-weight-light mb-4 blog-description">
                 {{ $post->excerpt }}
             </h2>
