4cd0bd61fd54d45b75118a5d22e64ec2566247b4
Omar Barbosa <omar.barbosa89@outlook.com>
2019-02-06 22:31:04 -0500

    Update post template

--- resources/views/templates/post.blade.php
+++ resources/views/templates/post.blade.php
@@ -137,18 +137,17 @@
 @section('scripts')
 
     <script async>
-    (function() {
-        var d = document, s = d.createElement('script');
-        s.src = 'https://omarbarbosa.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
+        (function() {
+            var d = document, s = d.createElement('script');
+            s.src = 'https://omarbarbosa.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
         })();
     </script>
-    {{-- <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>     --}}
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>    
     <script async>
         setTimeout(function(){
             $('#subscription').modal('show')
         }, 20000);
     </script>
-    {{-- <script async>hljs.initHighlightingOnLoad();</script> --}}
 @endsection
