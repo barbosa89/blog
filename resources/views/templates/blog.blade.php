a4c3ae408ecbe8ccdee453cc0763f5e89f42bc78
barbosa <omar.barbosa89@outlook.com>
2019-06-19 12:52:31 -0500

    Change styles in blog

--- resources/views/templates/blog.blade.php
+++ resources/views/templates/blog.blade.php
@@ -2,41 +2,10 @@
 
 @section('content')
     <!-- Header -->
-    <header class="blog-masthead bg-primary text-white">
-        <div class="container">
-            <div class="row text-center text-lg-left text-xl-left">
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-3 col-xl-3">
-                    <a href="{{ route('posts.article', ['slug' => $latest->slug]) }}"><img class="img-fluid mb-5" src="{{ empty($latest->featured_image) ? asset('images/article.png') : url($latest->featured_image) }}" alt="{{ $latest->title }}"></a>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-9 col-xl-9 text-md-center text-lg-left align-items-center">
-                        <a href="{{ route('posts.article', ['slug' => $latest->slug]) }}" class="text-white"><h1 class="text-uppercase">{{ $latest->title }}</h1></a>
-                        <p><i class="fas fa-calendar"></i> {{ $latest->created_at->toDateString() }}</p>
-                            <a href="{{ route('posts.article', ['slug' => $latest->slug]) }}" class="text-white">
-                        <h2 class="font-weight-light mb-4 text-justify">
-                            {{ $latest->excerpt }}
-                        </h2>
-                    </a>
-                    <div class="row align-items-end">
-                        <div class="col-4">
-                            <a href="hhttps://twitter.com/home?status={{ route('posts.article', ['slug' => $latest->slug]) }}">
-                                <i class="fab fa-twitter fa-2x"></i>
-                            </a>
-                        </div>
-                        <div class="col-4">
-                            <a href="https://www.facebook.com/sharer/sharer.php?u={{ route('posts.article', ['slug' => $latest->slug]) }}">
-                                <i class="fab fa-facebook fa-2x"></i>
-                            </a>
-                        </div>
-                        <div class="col-4">
-                        <a href="https://www.linkedin.com/shareArticle?mini=true&url={{ route('posts.article', ['slug' => $latest->slug]) }}&title={{ $latest->title }}&summary={{ $latest->excerpt }}&source=">
-                                <i class="fab fa-linkedin fa-2x"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>    
-        </div>
-    </header> 
+    @include('templates.blog-header', [
+        'post' => $latest,
+        'links' => true,
+    ])
 
     @if($posts->count() > 0)
         <div class="container blog">
--- resources/views/templates/empty.blade.php
+++ resources/views/templates/empty.blade.php
@@ -1,7 +1,7 @@
 <div class="container mt-4 mb-4">
     <div class="row">
         <div class="col-12">
-            <div class="card bg-primary text-white">
+            <div class="card text-gray">
                 <div class="card-body">
                     @lang('page.without_content')
                 </div>
