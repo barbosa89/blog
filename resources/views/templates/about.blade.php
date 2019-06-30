f5f51bfb85c2aad5c542b1b5e2bab6641ed92512
barbosa <omar.barbosa89@outlook.com>
2019-06-29 21:59:41 -0500

    Change H1 to H2 in post list, adding background images to Home page

--- resources/views/templates/about.blade.php
+++ resources/views/templates/about.blade.php
@@ -1,21 +1,21 @@
 <!-- About Section -->
-<section class="bg-primary text-white mb-0" id="about">
+<section class="bg-primary text-white mb-0 about-background" id="about">
     <div class="container">
-      <h2 class="text-center text-uppercase text-white">@lang('page.about')</h2>
-      <hr class="star-light mb-5">
-      <div class="row">
-        <div class="col-lg-4 ml-auto">
-          <p class="lead">@lang('page.me')</p>
+        <h2 class="text-center text-uppercase text-white mb-5">@lang('page.about')</h2>
+        {{-- <hr class="star-light mb-5"> --}}
+        <div class="row">
+            <div class="col-lg-4 ml-auto">
+                <p class="lead">@lang('page.me')</p>
+            </div>
+            <div class="col-lg-4 mr-auto">
+                <p class="lead">@lang('page.stack')</p>
+            </div>
         </div>
-        <div class="col-lg-4 mr-auto">
-          <p class="lead">@lang('page.stack')</p>
+        <div class="text-center mt-4">
+            <a class="btn btn-xl btn-outline-light" href="https://github.com/barbosa89" target="_blank">
+                <i class="fab fa-fw fa-github"></i>
+                @lang('page.projects')
+            </a>
         </div>
-      </div>
-      <div class="text-center mt-4">
-        <a class="btn btn-xl btn-outline-light" href="https://github.com/barbosa89" target="_blank">
-            <i class="fab fa-fw fa-github"></i>
-            @lang('page.projects')
-        </a>
-      </div>
     </div>
-  </section>
+</section>
--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,9 +1,9 @@
 <!-- Header -->
-<header class="masthead bg-primary text-white text-center">
+<header class="masthead bg-primary text-white text-center bg-background">
     <div class="container">
         <img class="img-fluid mb-5 d-block mx-auto" src="images/profile.png" alt="Omar Barbosa">
-        <h1 class="text-uppercase mb-0">Omar Barbosa</h1>
-        <hr class="star-light">
+        <h1 class="text-uppercase mb-5">Omar Barbosa</h1>
+        {{-- <hr class="star-light"> --}}
         <h2 class="font-weight-light mb-0">@lang('page.dev') - Full Stack</h2>
     </div>
 </header>
