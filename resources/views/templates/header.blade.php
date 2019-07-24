1a5a2cde0f125f50f0aca4cc31d595c6bd63538d
barbosa <omar.barbosa89@outlook.com>
2019-07-23 22:45:10 -0500

    Definitive icon in header, updating projects icons

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,8 +1,11 @@
 <!-- Header -->
 <header class="masthead bg-primary text-white text-center bg-background">
     <div class="container">
-        <img class="img-fluid mb-5 d-block mx-auto" src="images/profile.png" alt="Omar Barbosa">
-        <h1 class="text-uppercase mb-5">Omar Barbosa</h1>
+        <div class="icon mx-auto mb-5">
+            <p>&beta;</p>
+        </div>
+        {{-- <img class="img-fluid mb-5 d-block mx-auto" src="{{ asset('images/icon.png') }}" alt="Omar Barbosa"> --}}
+        <h1 class="text-uppercase mb-5" data-text="Omar Barbosa">Omar Barbosa</h1>
         {{-- <hr class="star-light"> --}}
         <h2 class="font-weight-light mb-0">@lang('page.dev') - Full Stack</h2>
     </div>
