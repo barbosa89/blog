eaff4a1bf6442aa90091b15d8b7d739dd38a26ac
barbosa <omar.barbosa89@outlook.com>
2019-07-24 13:20:23 -0500

    Icons of projects

--- resources/views/templates/portfolio-modals.blade.php
+++ resources/views/templates/portfolio-modals.blade.php
@@ -1,139 +1,139 @@
-          <!-- Portfolio Modals -->
-      
-          <!-- Portfolio Modal 1 -->
-          <div class="portfolio-modal mfp-hide" id="portfolio-modal-1">
-                <div class="portfolio-modal-dialog bg-white">
-                  <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                    <i class="fa fa-3x fa-times"></i>
-                  </a>
-                  <div class="container text-center">
-                    <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                        <h2 class="text-secondary text-uppercase mb-0">Simplexws</h2>
-                        <hr class="star-dark mb-5">
-                        <img class="img-fluid mb-5" src="{{ asset('images/portfolio/book-1.png') }}" alt="">
-                        <p class="mb-5">@lang('page.project_one')</p>
-                        <p class="mb-5">@lang('page.link'): <a href="https://simplexws.com" target="_blank">Simplexws</a></p>
-                        <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                          <i class="fa fa-close"></i>
-                          @lang('page.close_project')</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-          
-              <!-- Portfolio Modal 2 -->
-              <div class="portfolio-modal mfp-hide" id="portfolio-modal-2">
-                <div class="portfolio-modal-dialog bg-white">
-                  <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                    <i class="fa fa-3x fa-times"></i>
-                  </a>
-                  <div class="container text-center">
-                    <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                        <h2 class="text-secondary text-uppercase mb-0">Posada Sueño Real</h2>
-                        <hr class="star-dark mb-5">
-                        <img class="img-fluid mb-5" src="{{ asset('images/portfolio/hotel-1.png') }}" alt="">
-                        <p class="mb-5">@lang('page.project_two')</p>
-                        <p class="mb-5">@lang('page.link'): <a href="https://posadasuenoreal.com" target="_blank">Posada Sueño Real</a></p>
-                        <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                          <i class="fa fa-close"></i>
-                          @lang('page.close_project')</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-          
-              <!-- Portfolio Modal 3 -->
-              <div class="portfolio-modal mfp-hide" id="portfolio-modal-3">
-                <div class="portfolio-modal-dialog bg-white">
-                  <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                    <i class="fa fa-3x fa-times"></i>
-                  </a>
-                  <div class="container text-center">
-                    <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                        <h2 class="text-secondary text-uppercase mb-0">Cotízate</h2>
-                        <hr class="star-dark mb-5">
-                        <img class="img-fluid mb-5" src="{{ asset('images/portfolio/store.png') }}" alt="">
-                        <p class="mb-5">@lang('page.project_three')</p>
-                        <p class="mb-5">@lang('page.link'): <a href="https://cotizate.co" target="_blank">Cotízate</a></p>
-                        <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                          <i class="fa fa-close"></i>
-                          @lang('page.close_project')</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-          
-              <!-- Portfolio Modal 4 -->
-              <div class="portfolio-modal mfp-hide" id="portfolio-modal-4">
-                <div class="portfolio-modal-dialog bg-white">
-                  <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                    <i class="fa fa-3x fa-times"></i>
-                  </a>
-                  <div class="container text-center">
-                    <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                        <h2 class="text-secondary text-uppercase mb-0">Trasercol</h2>
-                        <hr class="star-dark mb-5">
-                        <img class="img-fluid mb-5" src="{{ asset('images/portfolio/truck-1.png') }}" alt="">
-                        <p class="mb-5">@lang('page.project_four')</p>
-                        <p class="mb-5">@lang('page.link'): <a href="https://trasercol.co" target="_blank">Trasercol</a></p>
-                        <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                          <i class="fa fa-close"></i>
-                          @lang('page.close_project')</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-          
-              <!-- Portfolio Modal 5 -->
-              <div class="portfolio-modal mfp-hide" id="portfolio-modal-5">
-                <div class="portfolio-modal-dialog bg-white">
-                  <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                    <i class="fa fa-3x fa-times"></i>
-                  </a>
-                  <div class="container text-center">
-                    <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                        <h2 class="text-secondary text-uppercase mb-0">Welkome</h2>
-                        <hr class="star-dark mb-5">
-                        <img class="img-fluid mb-5" src="{{ asset('images/portfolio/hotel-2.png') }}" alt="">
-                        <p class="mb-5">@lang('page.project_five')</p>
-                        <p class="mb-5">@lang('page.link'): <a href="https://github.com/barbosa89/welkome" target="_blank">Welkome</a></p>
-                        <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                          <i class="fa fa-close"></i>
-                          @lang('page.close_project')</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-          
-              <!-- Portfolio Modal 6 -->
-              <div class="portfolio-modal mfp-hide" id="portfolio-modal-6">
-                <div class="portfolio-modal-dialog bg-white">
-                  <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                    <i class="fa fa-3x fa-times"></i>
-                  </a>
-                  <div class="container text-center">
-                    <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                        <h2 class="text-secondary text-uppercase mb-0">@lang('page.other_projects')</h2>
-                        <hr class="star-dark mb-5">
-                        <img class="img-fluid mb-5" src="{{ asset('images/portfolio/github.png') }}" alt="">
-                        <p class="mb-5">@lang('page.project_six')</p>
-                        <p class="mb-5">@lang('page.link'): <a href="https://github.com/barbosa89" target="_blank">@lang('page.other_projects')</a></p>
-                        <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                          <i class="fa fa-close"></i>
-                          @lang('page.close_project')</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+<!-- Portfolio Modals -->
+
+<!-- Portfolio Modal 1 -->
+<div class="portfolio-modal mfp-hide" id="portfolio-modal-1">
+    <div class="portfolio-modal-dialog bg-white">
+        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+        <i class="fa fa-3x fa-times"></i>
+        </a>
+        <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+            <h2 class="text-secondary text-uppercase mb-0">Simplexws</h2>
+            <hr class="star-dark mb-5">
+            <img class="img-fluid mb-5" src="{{ asset('images/portfolio/book-1.png') }}" alt="">
+            <p class="mb-5">@lang('page.project_one')</p>
+            <p class="mb-5">@lang('page.link'): <a href="https://simplexws.com" target="_blank">Simplexws</a></p>
+            <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                <i class="fa fa-close"></i>
+                @lang('page.close_project')</a>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <!-- Portfolio Modal 2 -->
+    <div class="portfolio-modal mfp-hide" id="portfolio-modal-2">
+    <div class="portfolio-modal-dialog bg-white">
+        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+        <i class="fa fa-3x fa-times"></i>
+        </a>
+        <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+            <h2 class="text-secondary text-uppercase mb-0">Posada Sueño Real</h2>
+            <hr class="star-dark mb-5">
+            <img class="img-fluid mb-5" src="{{ asset('images/portfolio/hotel-1.png') }}" alt="">
+            <p class="mb-5">@lang('page.project_two')</p>
+            <p class="mb-5">@lang('page.link'): <a href="https://posadasuenoreal.com" target="_blank">Posada Sueño Real</a></p>
+            <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                <i class="fa fa-close"></i>
+                @lang('page.close_project')</a>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <!-- Portfolio Modal 3 -->
+    <div class="portfolio-modal mfp-hide" id="portfolio-modal-3">
+    <div class="portfolio-modal-dialog bg-white">
+        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+        <i class="fa fa-3x fa-times"></i>
+        </a>
+        <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+            <h2 class="text-secondary text-uppercase mb-0">Cotízate</h2>
+            <hr class="star-dark mb-5">
+            <img class="img-fluid mb-5" src="{{ asset('images/portfolio/store.png') }}" alt="">
+            <p class="mb-5">@lang('page.project_three')</p>
+            <p class="mb-5">@lang('page.link'): <a href="https://cotizate.co" target="_blank">Cotízate</a></p>
+            <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                <i class="fa fa-close"></i>
+                @lang('page.close_project')</a>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <!-- Portfolio Modal 4 -->
+    <div class="portfolio-modal mfp-hide" id="portfolio-modal-4">
+    <div class="portfolio-modal-dialog bg-white">
+        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+        <i class="fa fa-3x fa-times"></i>
+        </a>
+        <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+            <h2 class="text-secondary text-uppercase mb-0">Trasercol</h2>
+            <hr class="star-dark mb-5">
+            <img class="img-fluid mb-5" src="{{ asset('images/portfolio/truck-1.png') }}" alt="">
+            <p class="mb-5">@lang('page.project_four')</p>
+            <p class="mb-5">@lang('page.link'): <a href="https://trasercol.co" target="_blank">Trasercol</a></p>
+            <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                <i class="fa fa-close"></i>
+                @lang('page.close_project')</a>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <!-- Portfolio Modal 5 -->
+    <div class="portfolio-modal mfp-hide" id="portfolio-modal-5">
+    <div class="portfolio-modal-dialog bg-white">
+        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+        <i class="fa fa-3x fa-times"></i>
+        </a>
+        <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+            <h2 class="text-secondary text-uppercase mb-0">Welkome</h2>
+            <hr class="star-dark mb-5">
+            <img class="img-fluid mb-5" src="{{ asset('images/portfolio/hotel-2.png') }}" alt="">
+            <p class="mb-5">@lang('page.project_five')</p>
+            <p class="mb-5">@lang('page.link'): <a href="https://github.com/barbosa89/welkome" target="_blank">Welkome</a></p>
+            <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                <i class="fa fa-close"></i>
+                @lang('page.close_project')</a>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+
+    <!-- Portfolio Modal 6 -->
+    <div class="portfolio-modal mfp-hide" id="portfolio-modal-6">
+    <div class="portfolio-modal-dialog bg-white">
+        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+        <i class="fa fa-3x fa-times"></i>
+        </a>
+        <div class="container text-center">
+        <div class="row">
+            <div class="col-lg-8 mx-auto">
+            <h2 class="text-secondary text-uppercase mb-0">@lang('page.other_projects')</h2>
+            <hr class="star-dark mb-5">
+            <img class="img-fluid mb-5" src="{{ asset('images/portfolio/github.png') }}" alt="">
+            <p class="mb-5">@lang('page.project_six')</p>
+            <p class="mb-5">@lang('page.link'): <a href="https://github.com/barbosa89" target="_blank">@lang('page.other_projects')</a></p>
+            <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                <i class="fa fa-close"></i>
+                @lang('page.close_project')</a>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
