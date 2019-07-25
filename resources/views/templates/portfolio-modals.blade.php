e876d347e5cfb6a0e23abbe4f3ea3fe75e64dbc9
barbosa <omar.barbosa89@outlook.com>
2019-07-24 20:27:28 -0500

    Updating icons of projects, updating Telescope, updating project text

--- resources/views/templates/portfolio-modals.blade.php
+++ resources/views/templates/portfolio-modals.blade.php
@@ -55,11 +55,11 @@
         <div class="container text-center">
         <div class="row">
             <div class="col-lg-8 mx-auto">
-            <h2 class="text-secondary text-uppercase mb-0">Cotízate</h2>
+            <h2 class="text-secondary text-uppercase mb-0">Aol Soluciones</h2>
             <hr class="star-dark mb-5">
             <img class="img-fluid mb-5" src="{{ asset('images/portfolio/store.png') }}" alt="">
             <p class="mb-5">@lang('page.project_three')</p>
-            <p class="mb-5">@lang('page.link'): <a href="https://cotizate.co" target="_blank">Cotízate</a></p>
+            <p class="mb-5">@lang('page.link'): <a href="https://aolsoluciones.com" target="_blank">Tienda Aol</a></p>
             <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
                 <i class="fa fa-close"></i>
                 @lang('page.close_project')</a>
