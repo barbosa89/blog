5eecb53a3b59ef63b48ef92e583d905f7f7d8267
barbosa <omar.barbosa89@outlook.com>
2020-12-12 15:46:04 -0500

    L8 routes, refresh Wink installation

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\SubscriptionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,27 +32,27 @@ Route::get('/offline', function () {
     return view('modules/laravelpwa/offline');
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/blog', 'BlogController@index')->name('blog');
+Route::get('/blog', [BlogController::class, 'index'])->name('blog');
 
-Route::get('/tags/{tag}', 'BlogController@tags')->name('posts.tag');
-Route::get('/posts/{slug}', 'BlogController@article')->name('posts.article');
-Route::get('/search', 'BlogController@search')->name('posts.search');
+Route::get('/tags/{tag}', [BlogController::class, 'tags'])->name('posts.tag');
+Route::get('/posts/{slug}', [BlogController::class, 'article'])->name('posts.article');
+Route::get('/search', [BlogController::class, 'search'])->name('posts.search');
 
-Route::post('/subscribe', 'SubscriptionController@subscribe')
+Route::post('/subscribe', [SubscriptionController::class, 'subscribe'])
     ->name('subscribe')
     ->middleware(['sanitize', 'honeypot']);
 
-Route::get('/unsubscribe/{email}', 'SubscriptionController@unsubscribe')
+Route::get('/unsubscribe/{email}', [SubscriptionController::class, 'unsubscribe'])
     ->name('unsubscribe')
     ->middleware(['sanitize', 'honeypot']);
 
-Route::post('/message', 'ContactController@message')
+Route::post('/message', [ContactController::class, 'message'])
     ->name('message')
     ->middleware(['sanitize', 'honeypot']);
 
-Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index')
+Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index'])
     ->middleware('auth');
 
 
