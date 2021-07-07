ec838a6c967ac9bbf21a76d2606785afe66684b7
Barbosa89 <contacto@omarbarbosa.com>
2021-07-06 23:31:19 -0500

    Refactor: Add divider, remove height size

--- resources/js/components/ads/Feed.vue
+++ resources/js/components/ads/Feed.vue
@@ -1,11 +1,14 @@
 <template>
-    <div :id="reference" class="row mb-2 h-100">
-        <ins class="adsbygoogle col-12 h-100"
-            style="display:block"
-            data-ad-format="fluid"
-            data-ad-layout-key="-fo-2p+5v-nz+n3"
-            data-ad-client="ca-pub-2847498886705900"
-            data-ad-slot="9348019903"></ins>
+    <div>
+        <div class="row blog-divider"></div>
+        <div :id="reference" class="row mb-2">
+            <ins class="adsbygoogle col-12 h-100"
+                style="display:block"
+                data-ad-format="fluid"
+                data-ad-layout-key="-fo-2p+5v-nz+n3"
+                data-ad-client="ca-pub-2847498886705900"
+                data-ad-slot="9348019903"></ins>
+        </div>
     </div>
 </template>
 
