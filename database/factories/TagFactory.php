6c9d16bcc99aaed29149ff29022e63a5aca1c808
Barbosa <omar.barbosa@evertecinc.com>
2022-02-28 21:08:22 -0500

    Refactor: Add model class

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -4,9 +4,17 @@ namespace Database\Factories;
 
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Wink\WinkTag;
 
 class TagFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = WinkTag::class;
+
     /**
      * Define the model's default state.
      *
