c8c64a8eecfb17f9d63ada0cece47daa312afd7c
Omar Barbosa <omar.barbosa@evertecinc.com>
2025-06-17 14:33:26 -0500

    refactor: replace htmlentities with htmlspecialchars for input sanitization

--- app/Http/Middleware/InputSanitize.php
+++ app/Http/Middleware/InputSanitize.php
@@ -9,13 +9,18 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use function array_walk_recursive;
-use function htmlentities;
+use function htmlspecialchars;
 use function in_array;
 use function mb_strtolower;
 use function strip_tags;
 
 class InputSanitize
 {
+    /**
+     * @var array<int, string>
+     */
+    protected array $except = [];
+
     /**
      * Handle an incoming request.
      *
@@ -28,9 +33,16 @@ class InputSanitize
         }
 
         $input = $request->all();
+        $except = property_exists($this, 'except') ? $this->except : [];
 
-        array_walk_recursive($input, function (&$input): void {
-            $input = htmlentities(strip_tags($input), ENT_QUOTES, 'UTF-8');
+        array_walk_recursive($input, function (&$value, $key) use ($except): void {
+            if (in_array($key, $except, true)) {
+                return;
+            }
+
+            if (is_string($value)) {
+                $value = htmlspecialchars(strip_tags($value), ENT_QUOTES, 'UTF-8');
+            }
         });
 
         $request->merge($input);
